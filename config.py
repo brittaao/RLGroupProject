0495ce6c1e9ece241a1111a9fef109142cec4453
IngaKristin <inga_wohlert@web.de>
2023-05-04 13:27:54 +0200

    model10gone

--- config.py
+++ config.py
@@ -20,15 +20,17 @@ CartPole = {
     'memory_size': 50000,
     'n_episodes': 1000,
     'batch_size': 32,
-    'target_update_frequency': 100,
+    'target_update_frequency': 40,
     'train_frequency': 1,
-    'gamma': 0.95,
+    'gamma': 0.99,
     'lr': 1e-4,
-    'eps_start': 1.0,
+    'eps_start': 0.8,
     'eps_end': 0.05,
     'anneal_length': 10**4,
     'n_actions': 2,
 }
+
+
 # Hyperparameters for Pong-v5
 Pong = {
     'observation_stack_size': 4,
